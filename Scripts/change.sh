a5efc520b21a165419268e83b2747a971be20384
Jordan831 <vicky.electron831@gmail.com>
2021-03-10 11:55:08 +0530

    Added

--- Scripts/change.sh
+++ Scripts/change.sh
@@ -4,9 +4,8 @@ sudo service nginx restart
 cd /
 cd /var/www/public_html
 
-
 sudo chmod -R 777 /var/www/public_html/client
 sudo chmod -R 777 /var/www/public_html/client/node_modules
-cd /var/www/public_html/client
+
 
 
--- Scripts/del.sh
+++ Scripts/del.sh
@@ -4,5 +4,4 @@ set -e
 cd /
 cd /var/www/public_html
 sudo chmod -R 777 /var/www/public_html
-sudo chmod -R 777 /var/www/public_html/client
-sudo chmod -R 777 /var/www/public_html/client/node_modules
+
